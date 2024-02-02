77ae23d35409ae72e2df12890e7988c04016d5be
ocramgit <marstrabalhar@gmail.com>
2024-02-02 11:53:34 +0000

    first commit

---
+++ functions.js
@@ -0,0 +1,9 @@
+const getGameButton = document.getElementsByClassName("game");
+
+let gameLetters = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F"];
+
+function randomValue() {
+  return Math.random() * gameLetters.length;
+}
+
+console.log(randomValue);
